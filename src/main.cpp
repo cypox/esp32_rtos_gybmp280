f17c1fba3a8191fe1ca096f7c537e4eba1cd77aa
cypox <am_neggaz@esi.dz>
2023-01-28 01:16:37 +0100

    added mpu transmission

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
+#include <Adafruit_MPU6050.h>
 
 
 #define LED_BUILTIN 33
@@ -13,10 +14,11 @@
 #define I2C_SDA 14
 #define I2C_SCL 15
 
-TwoWire I2CSensors = TwoWire(0);
+TwoWire I2CWire = TwoWire(0);
 
 // BMP280 (Using I2C)
-Adafruit_BMP280 bmp(&I2CSensors);
+Adafruit_BMP280 bmp(&I2CWire);
+Adafruit_MPU6050 mpu;
 
 // define two tasks for Blink & AnalogRead
 void ServerHandler( void *pvParameters );
@@ -48,7 +50,78 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  I2CSensors.begin(I2C_SDA, I2C_SCL, 100000);
+  I2CWire.begin(I2C_SDA, I2C_SCL, 100000);
+
+  // Try to initialize!
+  mpu.begin(MPU6050_I2CADDR_DEFAULT, &I2CWire);
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+  delay(100);
 
   unsigned status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
   if (!status) {
@@ -165,6 +238,30 @@ void ReadSensors(void *pvParameters) // This is a task.
 
   for (;;)
   {
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    /* Print out the values */
+    Serial.print("Acceleration X: ");
+    Serial.print(a.acceleration.x);
+    Serial.print(", Y: ");
+    Serial.print(a.acceleration.y);
+    Serial.print(", Z: ");
+    Serial.print(a.acceleration.z);
+    Serial.println(" m/s^2");
+
+    Serial.print("Rotation X: ");
+    Serial.print(g.gyro.x);
+    Serial.print(", Y: ");
+    Serial.print(g.gyro.y);
+    Serial.print(", Z: ");
+    Serial.print(g.gyro.z);
+    Serial.println(" rad/s");
+
+    Serial.print("Temperature: ");
+    Serial.print(temp.temperature);
+    Serial.println(" degC");
+
     Serial.printf("Temperature = %.0f °C\n", bmp.readTemperature());
     Serial.printf("Pressure = %.2f hPa\n", bmp.readPressure()/100);
     Serial.printf("Approx altitude = %.2f m\n\n", bmp.readAltitude(1021)); /* Adjusted to local forecast! */
