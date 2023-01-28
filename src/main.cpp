ac4fd943e03f85aaa2bcec20347ba7eae64a92cf
cypox <am_neggaz@esi.dz>
2023-01-28 14:31:19 +0100

    removed multiline comments

--- src/main.cpp
+++ src/main.cpp
@@ -133,12 +133,13 @@ void setup() {
     while (1) delay(10);
   }
 
-  /* Default settings from datasheet. */
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+  // Default settings from datasheet.
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Operating Mode.
+                  Adafruit_BMP280::SAMPLING_X2,     // Temp. oversampling
+                  Adafruit_BMP280::SAMPLING_X16,    // Pressure oversampling
+                  Adafruit_BMP280::FILTER_X16,      // Filtering.
+                  Adafruit_BMP280::STANDBY_MS_500); // Standby time.
+
 
   auto res = esp32cam::Resolution::find(1024, 768);
   esp32cam::Config cfg;
